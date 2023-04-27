050b44234779b58a06afe255784b465bbfde6a88
Nikolai Orlov <semberk1297@gmail.com>
2023-04-27 16:38:41 +0300

    first version

---
+++ FindMinElementUsingRecursion.java
@@ -0,0 +1,27 @@
+public class FindMinElementUsingRecursion {
+    public static void main(String[] args) {
+        int[] array = {1, -2, -8, 0, 5};
+        System.out.println(findMin(array));
+        System.out.println(findMin(array, 1, array[0]));
+    }
+
+    private static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+
+    private static int findMin(int[] array, int i, int min) {
+        if (i == array.length) {
+            return min;
+        } else {
+            return findMin(array, i + 1, Math.min(array[i], min));
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Find min element in an array via recursion
